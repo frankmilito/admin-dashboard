ee6ce8f246d96756620a22e9f690bcf283a931ad
Franklin Obidike <Franklin@Obi-Franklin.local>
2022-03-07 23:04:56 +0100

    product list module

--- src/dummydata.jsx
+++ src/dummydata.jsx
@@ -49,6 +49,29 @@ export const data = [
   },
 ]
 
+export const product = [
+  {
+    name: 'Jan',
+    'Active Users': 3000,
+  },
+  {
+    name: 'Feb',
+    'Active Users': 2000,
+  },
+  {
+    name: 'Mar',
+    'Active Users': 4000,
+  },
+  {
+    name: 'Apr',
+    'Active Users': 1000,
+  },
+  {
+    name: 'May',
+    'Active Users': 4000,
+  },
+  
+]
 export const rows = [
   {
     id: 1,
@@ -147,6 +170,109 @@ export const rows = [
       'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
     status: 'active',
     transaction: '$400 .00',
+    email: 'mail@maiol.com',
+  },
+]
+
+
+export const productData = [
+  {
+    id: 1,
+    name: 'Apple Airpods',
+    avatar:
+      'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
+    status: 'active',
+    amount: '$148.00',
+    email: 'Jsnow@gmaill.com',
+  },
+  {
+    id: 2,
+    name: 'Apple Airpods',
+    avatar:
+      'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
+    status: 'active',
+    amount: '$298.00',
+    email: 'a@mail.com',
+  },
+  {
+    id: 3,
+    name: 'Apple Airpods',
+    avatar:
+      'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
+    status: 'active',
+    amount: '$453.00',
+    email: 'c@mail.com',
+  },
+  {
+    id: 4,
+    name: 'Apple Airpods',
+    avatar:
+      'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
+    status: 'active',
+    amount: '$148.00',
+    email: 'p@mail.com',
+  },
+  {
+    id: 5,
+    name: 'Apple Airpods',
+    avatar:
+      'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
+    status: 'active',
+    amount: '$148.00',
+    email: 'mail@maiol.com',
+  },
+  {
+    id: 6,
+    name: 'Apple Airpods',
+    avatar:
+      'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
+    status: 'active',
+    amount: '$148.00',
+    email: 'mail@maiol.com',
+  },
+  {
+    id: 7,
+    name: 'Apple Airpods',
+    avatar:
+      'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
+    status: 'active',
+    amount: '$148.00',
+    email: 'mail@maiol.com',
+  },
+  {
+    id: 8,
+    name: 'Manita queen',
+    avatar:
+      'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
+    status: 'active',
+    amount: '$148.00',
+    email: 'mail@maiol.com',
+  },
+  {
+    id: 9,
+    name: 'Quuen cid',
+    avatar:
+      'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
+    status: 'active',
+    amount: '$177.00',
+    email: 'mail@maiol.com',
+  },
+  {
+    id: 10,
+    name: 'Benjamin lie',
+    avatar:
+      'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
+    status: 'active',
+    amount: '$200.00',
+    email: 'mail@maiol.com',
+  },
+  {
+    id: 11,
+    name: 'Uchimadara',
+    avatar:
+      'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbGV8ZW58MHx8MHx8&w=1000&q=80',
+    status: 'active',
+    amount: '$400 .00',
     email: 'mail@maiol.com',
   },
 ]
